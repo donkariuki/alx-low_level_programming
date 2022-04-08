8d257ee443ffc972a23ebe387521fb5634373447
donkariuki <donkariuki42@gmail.com>
2022-04-08 08:21:56 -0700

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Description: print to base of 10 without using char
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+
+for (i - 0; <= 9; ++i)
+putchar('0' + i);
+
+putchar('\n');
+
+return (0);
+}

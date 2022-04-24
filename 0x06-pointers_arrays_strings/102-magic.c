e0c3afbafb1eacdc6efc2ec4255ed48df6f463ed
donkariuki <donkariuki42@gmail.com>
2022-04-24 11:53:07 -0700

    task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
 	 * only one statement
 	 * youre not aloowed to code anytrhing else
 	 */
-	p[5] = 98;
+	*(p + 5) = 98;
 	/*..... sp this prints 98\n*/
 	printf("a[2] = %d\n", a[2]);
 	return (0);

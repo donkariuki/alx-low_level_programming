a8065c927a79816515417bb5ca1d840273ec213f
donkariuki <donkariuki42@gmail.com>
2022-04-24 12:04:41 -0700

    task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -32,7 +32,7 @@ void rev_string(char *n)
  * @size_r: buffer size
  * Return: pointer to calling function
  */
-char *infinite_add(char *n1, char *2, char *r, int size_r)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
@@ -50,7 +50,7 @@ char *infinite_add(char *n1, char *2, char *r, int size_r)
 		if (i < 0)
 			val1 = 0;
 		else 
-			val1 = *(n1 + 1) - '0';
+			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
 		else 
